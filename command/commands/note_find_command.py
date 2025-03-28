49d4de50f1dda3e606fd7c59555b0030c72fdd98
yarqui <y.pelykh@gmail.com>
2025-03-28 15:06:17 -0400

    edit find-note command to return the <title>: <text> format

--- command/commands/note_find_command.py
+++ command/commands/note_find_command.py
@@ -1,52 +1,59 @@
-from typing import List
+from typing import Tuple, List
 
 from command.command import Command
 from execution_context import ExecutionContext
 from notes import Title, Tag, Note
 from user_input import index_question
+from display import StylizedElements, ColorsConstants
 
 
 class NoteFineCommand(Command):
     @property
     def name(self):
-        return 'find-note'
+        return "find-note"
 
     @property
     def aliases(self):
-        return ['fn']
-
+        return ["fn"]
 
     @property
     def description(self):
-        return 'Find a note by title or tag'
+        return "Find a note by title or tag"
 
-
-    def run(self, _, context: ExecutionContext, __) -> Tup(str, bool):
+    def run(self, _, context: ExecutionContext, __) -> Tuple[str, bool]:
         options = [Title.__name__, Tag.__name__]
 
         for i, option in enumerate(options):
-            print(f'[{i}] {option}')
+            print(f"[{i}] {option}")
 
-
-        index = index_question('Select field to search: ', len(options) - 1)
+        index = index_question("Select field to search: ", len(options) - 1)
 
         if index is None:
-            return 'Exiting..', False
+            return "Exiting..", False
 
         field_name = options[index]
 
-        term = input(f'Type {field_name}: ')
+        term = input(f"Type {field_name}: ")
 
-        notes: list[Note] = []
+        notes: List[Note] = []
 
         if index == 0:
             notes = context.notebook.find(term)
         elif index == 1:
             notes = context.notebook.find_by_tag(term)
 
-        message = 'Nothing found'
+        message = "Nothing found"
 
         if len(notes) > 0:
-            message = '\n'.join([str(note) for note in notes])
+            message = "\n".join(
+                [f"{str(note.title)}: {str(note.text)}" for note in notes]
+            )
 
-        return message, False
+        StylizedElements.stylized_print(
+            message, style=ColorsConstants.HEADER_COLOR.value
+        )
+
+        return (
+            "",
+            False,
+        )
