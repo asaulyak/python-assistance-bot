2877e69ebd44a14cc214197f226fd83608bf262f
Roman <didukhroma@gmail.com>
2025-03-29 16:34:50 +0100

    feat: added autocomplete only for main commands

--- user_input/user_input.py
+++ user_input/user_input.py
@@ -1,6 +1,11 @@
-from display import StylizedElements, ColorsConstants
+from typing import List
 from rich.text import Text
 from rich.console import Console
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.styles import Style
+from prompt_toolkit.completion import Completer
+from display import  ColorsConstants
 
 def yes_no_question(question: str) -> bool:
     console = Console()
@@ -32,3 +37,21 @@ def index_question(question: str, max_index: int, min_index: int = 0, offer_quit
         index = input_index(response)
 
     return index
+
+
+def autocompelete_text(commands:List[str]) -> str:
+    '''
+    Create a completer (auto-suggests commands)
+    '''
+    # Create a completer (auto-suggests commands)
+    command_completer = WordCompleter(commands, ignore_case=True, sentence=True)
+    our_style = Style.from_dict({
+        "prompt": ColorsConstants.INPUT_COLOR.value,
+        "completion-menu.completion": "bg:#333333 #ffffff",  # Suggestion text
+        "completion-menu.completion.current": "bg:#00ff00 #000000",  # Highlighted selection
+    })
+
+    while True:
+        user_input = prompt("Enter command: ", completer=command_completer, style=our_style,)
+
+        return user_input
