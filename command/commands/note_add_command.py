557dddfa89ff26f7591132377a32f43842f555f3
yarqui <y.pelykh@gmail.com>
2025-03-30 10:57:30 -0400

    add styled inputs to note_add

--- command/commands/note_add_command.py
+++ command/commands/note_add_command.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from command.command import Command
-from display import StylizedElements
+from display import StylizedElements, ColorsConstants
 from execution_context import ExecutionContext
 from field import init_field
 from notes import Title, Text, Note
@@ -10,32 +10,43 @@ from notes import Title, Text, Note
 class NoteAddCommand(Command):
     @property
     def name(self):
-        return 'add-note'
-
+        return "add-note"
 
     @property
     def aliases(self):
-        return ['an']
-
+        return ["an"]
 
     @property
     def description(self):
-        return 'Add a note'
+        return "Add a note"
 
     def run(self, args: list[str], context: ExecutionContext, commands: List) -> bool:
         title = None
         text = None
 
         while not title or title.is_empty():
-            title = init_field(Title, input('Title: '))
+            title = init_field(
+                Title,
+                StylizedElements.stylized_input(
+                    "Title: ", ColorsConstants.INPUT_COLOR.value
+                ),
+            )
 
         while not text:
-            text = init_field(Text, input('Text: '))
+            text = init_field(
+                Text,
+                StylizedElements.stylized_input(
+                    "Text: ", ColorsConstants.INPUT_COLOR.value
+                ),
+            )
 
-        note = Note(title, text)
+        note_to_add = Note(title, text)
 
-        context.notebook.add(note)
+        context.notebook.add(note_to_add)
 
-        StylizedElements.stylized_print('Note added')
+        StylizedElements.stylized_print(
+            "Note added", style=ColorsConstants.SUCCESS_COLOR.value
+        )
+        stop = False
 
-        return False
+        return stop
