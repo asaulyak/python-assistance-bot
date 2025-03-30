395f2c6c6988a33f0d835b2b1497f21f85d8574d
yarqui <y.pelykh@gmail.com>
2025-03-30 10:42:10 -0400

    refactor note_delete

--- command/commands/note_delete_command.py
+++ command/commands/note_delete_command.py
@@ -1,41 +1,50 @@
-from typing import List
-
 from command.command import Command
-from display import StylizedElements
-from display.paginator import Paginator
+from notes import Notebook
+from display import StylizedElements, ColorsConstants
 from execution_context import ExecutionContext
-from user_input import index_question
 
 
 class NoteDeleteCommand(Command):
     @property
     def name(self):
-        return 'delete-note'
-
+        return "delete-note"
 
     @property
     def aliases(self):
-        return ['dn']
-
+        return ["dn"]
 
     @property
     def description(self):
-        return 'Delete note'
+        return "Delete note"
 
+    def run(self, _, context: ExecutionContext, __) -> bool:
+        notebook: Notebook = context.notebook
 
-    def run(self, args: list[str], context: ExecutionContext, commands: List) -> bool:
-        paginator = Paginator(context.notebook)
-        paginator.show()
+        if notebook.is_empty():
+            StylizedElements.stylized_print(
+                "No notes to delete. Please add a note first.",
+                ColorsConstants.HIGHLIGHT_COLOR.value,
+            )
 
-        index = index_question('Type index of a note (or \'q\' to cancel): ', len(context.notebook) - 1)
-
-        if index is None:
-            StylizedElements.stylized_print('No notes were deleted')
             return False
 
-        note = context.notebook[index]
-        context.notebook.delete(note)
+        options = [
+            f"{i + 1}. {note.get_title().value}" for i, note in enumerate(notebook)
+        ]
+        options.append("Cancel")
 
-        StylizedElements.stylized_print('Note deleted')
+        selected = StylizedElements.console_menu("Select a note to edit:", options)
+
+        if selected == "Cancel":
+            return False
+
+        selected_index = options.index(selected)
+        note_to_remove = notebook[selected_index]
+        notebook.delete(note_to_remove)
+
+        StylizedElements.stylized_print(
+            f"Deleted note: {note_to_remove.get_title().value}",
+            style=ColorsConstants.SUCCESS_COLOR.value,
+        )
 
         return False
